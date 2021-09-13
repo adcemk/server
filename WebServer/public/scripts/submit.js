8823ead8b85ba2938f6f3493f975c9f591070f0e
amaury <amaury.martinez5892@alumnos.udg.mx>
2021-09-13 14:25:17 -0500

    ideas

--- WebServer/public/scripts/submit.js
+++ WebServer/public/scripts/submit.js
@@ -14,6 +14,11 @@ function transfer(e){
   socket.onmessage = e => {
     var data = JSON.parse(e.data)
     if(data['type'] == 'horario') {
+      // do something with the schedules
+      //i.e.
+      //insert all of the objects into a list
+      //redirect with window.location to table.html with list as a parameter
+      //or (probably better just create json string of list and use sessionStorage to retrieve in other html)
       console.log(data)
     }
     else {
